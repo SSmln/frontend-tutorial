8670bef55ed6583d1f9f0d8667c8df148000ef55
SSmln <lsm7630@gmail.com>
2023-05-11 18:40:55 +0900

    7 commit

---
+++ react/todo-list/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Template from './components/Template';
+
+const App= ()=>{
+  return <Template>안녕하세요</Template>;
+};
+
+export default App;
---
+++ react/todo-list/src/components/Template.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Temaplate = () =>  {
+    return (<div>
+      <div> 오늘의 할 일 (0)</div>
+      <div> 여기는 할 일 목록이 들어갑니다</div>
+    </div>
+  );
+};
+
+export default Temaplate;
