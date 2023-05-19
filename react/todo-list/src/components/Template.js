c898f1deb3a23ffb474b2741751227a52a231c3d
SSmln <lsm7630@gmail.com>
2023-05-19 20:44:42 +0900

    react todolist 1

--- react/todo-list/src/components/Template.js
+++ react/todo-list/src/components/Template.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Temaplate = () =>  {
+const Temaplate = ({children}) =>  {
     return (<div>
       <div> 오늘의 할 일 (0)</div>
-      <div> 여기는 할 일 목록이 들어갑니다</div>
+      <div>{children}</div>
     </div>
   );
 };
